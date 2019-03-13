98120ae7e970d5ebfcb8100d7c5cbf8ba12af109
loupei <loupei@cnbmtech.com>
2019-03-13 17:05:31 +0800

    舞台搭建

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
@@ -11,7 +11,7 @@ imageDatas = (function genImageURL(imageDatasArr) {
   for (var i = 0,l = imageDatasArr.length; i < l; i++) {
     var singleImageData = imageDatasArr[i];
     
-    singleImageData.imageURL = require('../images' + singleImageData.fileName);
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
     
     imageDatasArr[i] = singleImageData;
   }
@@ -19,15 +19,38 @@ imageDatas = (function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas)
 
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+              alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 
 
 class AppComponent extends React.Component {
   render() {
+    
+    var controllerUnits = [],
+        imgFigurs = [];
+    
+    imageDatas.forEach(function (value) {
+      imgFigurs.push(<ImgFigure data={value} />)
+    })
+    
     return(
       <section className="stage">
         <section className="img-sec">
+          {imgFigurs}
         </section>
         <nav className="controller-nav">
+          {controllerUnits}
         </nav>
       </section>
     )
